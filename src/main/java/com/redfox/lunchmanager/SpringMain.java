1be0a8c7f5614a64923e5f9d7c67ffc1da6461f5
Vladislav Lisin <v1adis1av.lis@mail.ru>
2021-10-21 13:14:32 +0300

    Feature dependency injection

--- src/main/java/com/redfox/lunchmanager/SpringMain.java
+++ src/main/java/com/redfox/lunchmanager/SpringMain.java
@@ -1,10 +1,13 @@
 package com.redfox.lunchmanager;
 
-import com.redfox.lunchmanager.repository.RestaurantRepository;
 import com.redfox.lunchmanager.repository.inmemory.InMemoryDishRepository;
 import com.redfox.lunchmanager.repository.inmemory.InMemoryRestaurantRepository;
 import com.redfox.lunchmanager.repository.inmemory.InMemoryUserRepository;
 import com.redfox.lunchmanager.repository.inmemory.InMemoryVoteRepository;
+import com.redfox.lunchmanager.service.DishService;
+import com.redfox.lunchmanager.service.RestaurantService;
+import com.redfox.lunchmanager.service.UserService;
+import com.redfox.lunchmanager.service.VoteService;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -24,22 +27,27 @@ public class SpringMain {
         ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
         System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
 
-        InMemoryUserRepository userRepository = (InMemoryUserRepository) appCtx.getBean("inmemoryUserRepository");
+        InMemoryUserRepository userRepository = appCtx.getBean(InMemoryUserRepository.class);
         userRepository.init();
-        InMemoryRestaurantRepository restaurantRepository = (InMemoryRestaurantRepository) appCtx.getBean(RestaurantRepository.class);
+        InMemoryRestaurantRepository restaurantRepository = appCtx.getBean(InMemoryRestaurantRepository.class);
         restaurantRepository.init();
         InMemoryDishRepository dishRepository = appCtx.getBean(InMemoryDishRepository.class);
         dishRepository.init();
         InMemoryVoteRepository voteRepository = appCtx.getBean(InMemoryVoteRepository.class);
         voteRepository.init();
 
-        System.out.println("InMemoryUserRepository");
-        userRepository.getAll().forEach(System.out::println);
-        System.out.println("InMemoryRestaurantRepository");
-        restaurantRepository.getAll().forEach(System.out::println);
-        System.out.println("InMemoryDishRepository");
-        dishRepository.getAll(restaurant1.getId()).forEach(System.out::println);
-        System.out.println("InMemoryVoteRepository");
-        System.out.println(voteRepository.getByDate(LocalDate.now(), user1.getId()));
+        UserService userService = appCtx.getBean(UserService.class);
+        RestaurantService restaurantService = appCtx.getBean(RestaurantService.class);
+        DishService dishService = appCtx.getBean(DishService.class);
+        VoteService voteService = appCtx.getBean(VoteService.class);
+
+        System.out.println("UserService");
+        userService.getAll().forEach(System.out::println);
+        System.out.println("RestaurantService");
+        restaurantService.getAll().forEach(System.out::println);
+        System.out.println("DishService");
+        dishService.getAll(restaurant1.getId()).forEach(System.out::println);
+        System.out.println("VoteService");
+        System.out.println(voteService.getByDate(LocalDate.now(), user1.getId()));
     }
 }
