b6154d881491a1e55c934c13b099580f6c4a44b7
Vladislav Lisin <v1adis1av.lis@mail.ru>
2021-10-23 16:24:35 +0300

    Feature spring test api

---
+++ src/test/java/com/redfox/lunchmanager/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -0,0 +1,93 @@
+package com.redfox.lunchmanager.web.user;
+
+import com.redfox.lunchmanager.model.User;
+import com.redfox.lunchmanager.repository.inmemory.InMemoryUserRepository;
+import com.redfox.lunchmanager.util.exception.NotFoundException;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.redfox.lunchmanager.UserTestData.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringRunner.class)
+@Ignore
+public class InMemoryAdminRestControllerSpringTest {
+
+    @Autowired
+    private AdminRestController controller;
+    @Autowired
+    private InMemoryUserRepository repository;
+
+    @Before
+    public void setup() {
+        // re-initialize
+        repository.init();
+    }
+
+    @Test
+    public void create() {
+        User expected = getNew();
+        int id = controller.create(expected).getId();
+        assertEquals(expected, controller.get(id));
+    }
+
+    @Test
+    public void createNotNew() {
+        assertThrows(IllegalArgumentException.class, () -> controller.create(getUpdated()));
+    }
+
+    @Test
+    public void delete() {
+        controller.delete(USER_ID_1);
+        assertThrows(NotFoundException.class, () -> controller.get(USER_ID_1));
+    }
+
+    @Test
+    public void deleteNotFound() {
+        assertThrows(NotFoundException.class, () -> controller.delete(NOT_FOUND));
+    }
+
+    @Test
+    public void get() {
+        assertEquals(user1, controller.get(USER_ID_1));
+    }
+
+    @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> controller.get(NOT_FOUND));
+    }
+
+    @Test
+    public void getByMail() {
+        assertEquals(user3, controller.getByMail(user3.getEmail()));
+    }
+
+    @Test
+    public void getByMailNotFound() {
+        assertThrows(NotFoundException.class, () -> controller.getByMail("mail"));
+    }
+
+    @Test
+    public void getAll() {
+        assertEquals(users, controller.getAll());
+    }
+
+    @Test
+    public void update() {
+        User user = getUpdated();
+        controller.update(user, USER_ID_3);
+        assertEquals(user, controller.get(USER_ID_3));
+    }
+
+    @Test
+    public void updateAssureIdConsistent() {
+        assertThrows(IllegalArgumentException.class, () -> controller.update(getUpdated(), NOT_FOUND));
+    }
+}
