603ec6a4993d610516e269e2c943b27c024af4af
Vladislav Lisin <v1adis1av.lis@mail.ru>
2021-11-04 16:56:07 +0300

    Feature java common annotations api

--- src/test/java/com/redfox/lunchmanager/repository/inmemory/InMemoryUserRepository.java
+++ src/test/java/com/redfox/lunchmanager/repository/inmemory/InMemoryUserRepository.java
@@ -2,8 +2,11 @@ package com.redfox.lunchmanager.repository.inmemory;
 
 import com.redfox.lunchmanager.model.User;
 import com.redfox.lunchmanager.repository.UserRepository;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
@@ -11,16 +14,26 @@ import java.util.stream.Collectors;
 
 import static com.redfox.lunchmanager.UserTestData.*;
 import static com.redfox.lunchmanager.model.AbstractBaseEntity.START_SEQ;
+import static org.slf4j.LoggerFactory.getLogger;
 
 @Repository
 public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
+    private static final Logger log = getLogger(InMemoryUserRepository.class);
+
+    @PostConstruct
+    public void postConstruct() {
+        log.info("+++ PostConstruct");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        log.info("+++ PreDestroy");
+    }
 
     public void init() {
         map.clear();
-        map.put(USER_ID_1, user1);
-        map.put(USER_ID_2, user2);
-        map.put(USER_ID_3, user3);
-        counter.set(START_SEQ + 3);
+        users.forEach(user -> map.put(user.getId(), user));
+        counter.set(START_SEQ + users.size());
     }
 
     @Override
