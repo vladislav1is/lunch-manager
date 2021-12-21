b37299e482d6cc89ea2ee069041c422fac48289c
Vladislav Lisin <v1adis1av.lis@mail.ru>
2021-12-21 05:16:50 +0300

    Fix ValidationUtil.java

--- src/main/java/com/redfox/lunchmanager/util/ValidationUtil.java
+++ src/main/java/com/redfox/lunchmanager/util/ValidationUtil.java
@@ -1,6 +1,6 @@
 package com.redfox.lunchmanager.util;
 
-import com.redfox.lunchmanager.model.AbstractBaseEntity;
+import com.redfox.lunchmanager.HasId;
 import com.redfox.lunchmanager.util.exception.NotFoundException;
 import org.springframework.core.NestedExceptionUtils;
 import org.springframework.lang.NonNull;
@@ -49,24 +49,24 @@ public final class ValidationUtil {
         }
     }
 
-    public static void checkNew(AbstractBaseEntity entity) {
-        if (!entity.isNew()) {
-            throw new IllegalArgumentException(entity + " must be new (id=null)");
+    public static void checkNew(HasId bean) {
+        if (!bean.isNew()) {
+            throw new IllegalArgumentException(bean + " must be new (id=null)");
         }
     }
 
-    public static void assureIdConsistent(AbstractBaseEntity entity, int id) {
-//        conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
-        if (entity.isNew()) {
-            entity.setId(id);
-        } else if (entity.getId() != id) {
-            throw new IllegalArgumentException(entity + " must be with id=" + id);
+    public static void assureIdConsistent(HasId bean, int id) {
+        // conservative when you reply, but accept liberally (http://stackoverflow.com/a/32728226/548473)
+        if (bean.isNew()) {
+            bean.setId(id);
+        } else if (bean.id() != id) {
+            throw new IllegalArgumentException(bean + " must be with id=" + id);
         }
     }
 
-    //  https://stackoverflow.com/a/65442410/548473
     @NonNull
     public static Throwable getRootCause(@NonNull Throwable t) {
+        //  https://stackoverflow.com/a/65442410/548473
         Throwable rootCause = NestedExceptionUtils.getRootCause(t);
         return rootCause != null ? rootCause : t;
     }
