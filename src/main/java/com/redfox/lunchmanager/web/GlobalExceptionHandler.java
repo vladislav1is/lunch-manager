6939e356caf7203a65c9ab5fb53ce6afec2bb1a9
Vladislav Lisin <v1adis1av.lis@mail.ru>
2021-12-31 02:01:47 +0300

    Feature global exception

---
+++ src/main/java/com/redfox/lunchmanager/web/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.redfox.lunchmanager.web;
+
+import com.redfox.lunchmanager.AuthorizedUser;
+import com.redfox.lunchmanager.util.ValidationUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        log.error("Exception at request " + req.getRequestURL(), e);
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        ModelAndView mav = new ModelAndView("exception",
+                Map.of("exception", rootCause, "message", rootCause.toString(), "status", httpStatus));
+        mav.setStatus(httpStatus);
+
+        //  Interceptor is not invoked, put userTo
+        AuthorizedUser authorizedUser = SecurityUtil.safeGet();
+        if (authorizedUser != null) {
+            mav.addObject("userTo", authorizedUser.getUserTo());
+        }
+        return mav;
+    }
+}
