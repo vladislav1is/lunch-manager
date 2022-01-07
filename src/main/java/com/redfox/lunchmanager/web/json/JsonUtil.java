1e18d36c2406d2a70425a83fb7296180b2a195eb
Vladislav Lisin <v1adis1av.lis@mail.ru>
2022-01-07 18:58:31 +0300

    Fix JsonUtil.java

--- src/main/java/com/redfox/lunchmanager/web/json/JsonUtil.java
+++ src/main/java/com/redfox/lunchmanager/web/json/JsonUtil.java
@@ -12,6 +12,9 @@ import static com.redfox.lunchmanager.web.json.JacksonObjectMapper.getMapper;
 
 public class JsonUtil {
 
+    private JsonUtil() {
+    }
+
     public static <T> List<T> readValues(String json, Class<T> clazz) {
         ObjectReader reader = getMapper().readerFor(clazz);
         try {
